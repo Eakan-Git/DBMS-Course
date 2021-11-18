a18747ee510c2c1e9d915bb650b820d97b4e58e6
Yen Nhi Le <leyennhi112@gmail.com>
2021-11-18 23:10:28 +0700

    Update Concurrency.sql

--- Application/Script/Concurrency.sql
+++ Application/Script/Concurrency.sql
@@ -1,4 +1,4 @@
-﻿select * from DONHANG where MaDH = N'DH0005'
+select * from DONHANG where MaDH = N'DH0005'
 
 create procedure TinhHuong1 (@MaDH nvarchar(10))
 as
@@ -7,7 +7,19 @@ begin
 	set TinhTrang = N'Đã hủy'
 	where MaDH = @MaDH
 
-	WaitFor Delay '00:00:10'	if N'Đã hủy' != (select TinhTrang						from DONHANG						where MaDH = @MaDH) 	begin		raiserror (N'Đơn hàng không hủy được!', 10, 1)	endendEXECUTE TinhHuong1 @MaDH = N'DH0005'--Rollback Transaction
+	WaitFor Delay '00:00:10'
+
+	if N'Đã hủy' != (select TinhTrang
+						from DONHANG
+						where MaDH = @MaDH) 
+	begin
+		raiserror (N'Đơn hàng không hủy được!', 10, 1)
+	end
+end
+
+EXECUTE TinhHuong1 @MaDH = N'DH0005'
+
+
 update DONHANG
 set TinhTrang = N'Đang giao hàng'
 where MaDH = N'DH0005'
