b62c3e46a58d6be02fe27692fedf71ac1509daa8
Yen Nhi Le <leyennhi112@gmail.com>
2021-11-22 21:04:11 +0700

    Update ProcedureList.sql

--- Application/Script/Procedure/ProcedureList.sql
+++ Application/Script/Procedure/ProcedureList.sql
@@ -68,3 +68,50 @@ BEGIN
     WHERE MASP = @MASP
 	COMMIT TRAN
 END
+
+--
+create procedure THEMSANPHAM
+    @MaSP nvarchar(10),
+    @MaCN nvarchar(10),
+    @TenSP nvarchar(150),
+    @Gia bigint,
+    @MoTa nvarchar(200)
+as
+begin
+    begin tran
+        if exists (select * from SANPHAM where @MaSP = MaSP)
+        begin
+            rollback tran
+            raiserror(N'Sản phẩm đã tồn tại!', 10, 1)
+        end
+
+        if not exists (select * from CHINHANH where @MaCN = MaCN)
+        begin
+            rollback tran
+            raiserror (N'Chi nhánh không tồn tại', 10, 1)
+        end
+
+        insert SANPHAM (MaSP, MaCN, TenSP, MoTa, Gia) values (@MaSP, @MaCN, @TenSP, @MoTa, @Gia)
+    commit tran
+end
+go
+
+execute THEMSANPHAM @MaSP = 'sp0000',  @MaCN = 'cn0000',  @TenSP = N'Chocopie', @MoTa = null, @Gia = 1000000   
+go
+
+create procedure KHACHHANG_XEMSANPHAM 
+	@Gia bigint
+as
+begin
+	begin tran
+		if not exists (select * from SANPHAM where @Gia = 1000000)
+		begin
+			rollback tran
+			raiserror (N'Sản phẩm không tồn tại', 10, 1)
+		end
+
+		SELECT * FROM SANPHAM WHERE Gia = 1000000
+	commit tran
+end
+go
+exec KHACHHANG_XEMSANPHAM @Gia = 1000000
