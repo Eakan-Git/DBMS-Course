e5bca2e0123a23747d7e973c771e5df67dee554a
Nguyễn Quang Trường <58219087+HCMUS-Eakan@users.noreply.github.com>
2021-11-22 22:40:24 +0700

    Update Tran_1_LostUpdate.sql

--- Application/Script/19127608/Tran_1_LostUpdate.sql
+++ Application/Script/19127608/Tran_1_LostUpdate.sql
@@ -1,7 +1,3 @@
-
---LOST UPDATE ERROR
---T1 Khách hàng hủy đơn hàng
-
 CREATE PROCEDURE KHACHHANG_HUY_DONHANG
 	@MaDH NVARCHAR(10),
 	@TinhTrang NVARCHAR(20)
@@ -14,27 +10,28 @@ BEGIN
 			ROLLBACK
 			raiserror(N'Đơn hàng không tồn tại', 16, 1)
         END
-		DECLARE @TinhTrang NVARCHAR(20)
+		DECLARE @TinhTrang2 NVARCHAR(20)
         --Kiểm tra xem đơn hàng đã giao chưa
-		SET @TinhTrang = (SELECT DONHANG.TinhTrang FROM DONHANG WHERE DONHANG.MaDH = @MaDH)
-		IF @TinhTrang = N'Đã giao hàng' --nếu đã giao thì không làm gì được nữa
+		SET @TinhTrang2 = (SELECT DONHANG.TinhTrang FROM DONHANG WHERE DONHANG.MaDH = @MaDH)
+		IF @TinhTrang2 = N'Đã giao hàng' --nếu đã giao thì không làm gì được nữa
 		BEGIN
 			ROLLBACK
 			raiserror(N'Đơn hàng đã giao, không thể cập nhật.', 16, 1)
 		END
 		ELSE
-		IF @TinhTrang = N'Đã hủy' --nếu đã hủy rồi thì không làm gì được nữa
+		IF @TinhTrang2 = N'Đã hủy' --nếu đã hủy rồi thì không làm gì được nữa
 		BEGIN
 			ROLLBACK
 			raiserror(N'Đơn hàng đã hủy, không thể cập nhật.', 16, 1)
 		END
-		WAITFOR DELAY '00:00:10'
 		UPDATE DONHANG
 		SET DONHANG.TinhTrang = @TinhTrang
 		WHERE DONHANG.MaDH = @MaDH
+		WAITFOR DELAY '00:00:10'
 	COMMIT TRAN
 END
 GO
 
 EXEC CAPNHAT_DONHANG @MaDH = N'DH4364', @TinhTrang = N'Đã hủy'
 GO
+SELECT * FROM DONHANG WHERE MaDH = N'DH4364'
