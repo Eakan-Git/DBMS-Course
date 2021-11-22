1106349471c7ca7145057c6aa4524f5f29646279
Yen Nhi Le <leyennhi112@gmail.com>
2021-11-22 21:16:27 +0700

    Update ProcedureList.sql

--- Application/Script/Procedure/ProcedureList.sql
+++ Application/Script/Procedure/ProcedureList.sql
@@ -109,3 +109,50 @@ END
 GO
 EXECUTE UPDATE_GIA @MASP =N'SP1922',
 @GIA = 99999  
+-----------------------------------------------------------------------------
+
+create procedure THEMSANPHAM
+    @MaSP nvarchar(10),
+    @MaCN nvarchar(10),
+    @TenSP nvarchar(150),
+	@Gia bigint,
+    @MoTa nvarchar(200)
+as
+begin
+    begin tran
+        if exists (select * from SANPHAM where @MaSP = MaSP)
+        begin
+            rollback tran
+            raiserror(N'Sản phẩm đã tồn tại!', 10, 1)
+        end
+
+        if not exists (select * from CHINHANH where @MaCN = MaCN)
+        begin
+            rollback tran
+            raiserror (N'Chi nhánh không tồn tại', 10, 1)
+        end
+
+        insert SANPHAM (MaSP, MaCN, TenSP, MoTa, Gia) values (@MaSP, @MaCN, @TenSP, @MoTa, @Gia)
+    commit tran
+end
+go
+
+execute THEMSANPHAM @MaSP = 'sp0000',  @MaCN = 'cn0000',  @TenSP = N'Chocopie', @MoTa = null, @Gia = 30000   
+go
+
+create procedure KHACHHANG_XEMSANPHAM 
+	@MaSP nvarchar(10)
+as
+begin
+	begin tran
+		if not exists (select * from SANPHAM where @MaSP = MaSP)
+		begin
+			rollback tran
+			raiserror (N'Sản phẩm không tồn tại', 10, 1)
+		end
+
+		SELECT * FROM SANPHAM WHERE MaSP = @MaSP 
+	commit tran
+end
+go
+exec KHACHHANG_XEMSANPHAM @MaSP = N'SP0008'
