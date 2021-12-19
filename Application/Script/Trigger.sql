0e29f18621180645cd3ce12a643cd9fff1442851
Yen Nhi Le <leyennhi112@gmail.com>
2021-12-19 14:13:47 +0700

    Update Trigger.sql

--- Application/Script/Trigger.sql
+++ Application/Script/Trigger.sql
@@ -1,8 +1,8 @@
-﻿use DBMS_ThucHanh_Nhom15
+use DBMS_ThucHanh_Nhom15
 go
 ----------------------------------------------------------------------------------------
 ---- 1) ThanhTien = (SoLuong * Gia) 
--- update SoLuong
+-- update SoLuong CHITIETDONHANG
 create trigger trigger_ThanhTien_updateSL
 on CHITIETDONHANG
 for insert, update, delete as
@@ -21,7 +21,7 @@ begin
 			ctdh.MaSP = sp.MaSP 
 end
 
--- update Gia
+-- update Gia SANPHAM
 create trigger trigger_ThanhTien_updateSP
 on SANPHAM
 for insert, update, delete as
@@ -42,6 +42,7 @@ end
 
 
 ---- 2) Tổng tiền = PhiVanChuyen + sum(CHITIETHOADON.ThanhTien)
+-- update CHITIETDONHANG
 create trigger trigger_TongTien
 on CHITIETDONHANG
 for insert, update, delete as
@@ -55,38 +56,42 @@ begin
 		exists (select * from deleted d where d.MaDH = DONHANG.MaDH) 
 end
 
+-- update PhiVanChuyen 
+create trigger trigger_TongTien_updatePVC
+on DONHANG
+for insert, update, delete as
+if update (PhiVanChuyen)
+begin
+	if exists (select dh.PhiVanChuyen from DONHANG dh where dh.PhiVanChuyen < 0)
+	begin
+		 raiserror(N'Phí vận chuyển không hợp lệ', 10, 1)
+		 rollback transaction
+	end
+	update DONHANG
+	set TongTien = PhiVanChuyen + (select sum(ctdh.ThanhTien)
+									from CHITIETDONHANG ctdh
+									where ctdh.MaDH = DONHANG.MaDH)
+	where 
+		exists (select * from inserted i where i.MaDH = DONHANG.MaDH) or
+		exists (select * from deleted d where d.MaDH = DONHANG.MaDH) 
+end
+
 ----------------------------------------------------------------------------------
--- Cập nhật ThanhTien và TongTien trước khi test Trigger
-update CHITIETDONHANG
-set ThanhTien = ctdh.SoLuong * sp.Gia
-from CHITIETDONHANG ctdh, SANPHAM sp
-where ctdh.MaSP = sp.MaSP 
+-- Test for triggers
+insert into SANPHAM (TenSP, Gia) values (N'Chocolate', 10000)
+insert into CHITIETDONHANG (MaDH, MaSP, SoLuong) values (N'1', N'2001', 10)
 
-update DONHANG
-set TongTien = PhiVanChuyen + (select sum(ctdh.ThanhTien)
-								from CHITIETDONHANG ctdh
-								where ctdh.MaDH = DONHANG.MaDH)
+delete from CHITIETDONHANG where MaDH = N'1' and MaSP = N'2001'
+delete from SANPHAM where MaSP = N'2001'
 
-----------------------------------------------------------------------------------
--- Test for trigger 
-insert into SANPHAM values (N'sp1001', N'CN662Y', N'Chocolate', 10000, N'Orange')
-insert into CHITIETDONHANG (MaDH, MaSP, SoLuong) values (N'DH0005', N'sp1001', 10)
+update CHITIETDONHANG set SoLuong = -3 where MaDH = N'1' and MaSP = N'2001'
 
-insert into SANPHAM values (N'sp1002', N'CN662Y', N'Hershey kisses', 13000, N'Pink')
-insert into CHITIETDONHANG (MaDH, MaSP, SoLuong) values (N'DH0005', N'sp1002', 15)
+update SANPHAM set Gia = 20000  where MaSP = N'2001' 
 
-delete from CHITIETDONHANG where MaDH = N'DH0005' and MaSP = N'sp1001'
-delete from SANPHAM where MaSP = N'sp1001'
+update SANPHAM set Gia = -10000 where MaSP = N'2001'
 
-delete from CHITIETDONHANG where MaDH = N'DH0005' and MaSP = N'sp1002'
-delete from SANPHAM where MaSP = N'sp1002'
+update DONHANG set PhiVanChuyen = -10 where MaDH = N'1'
 
-update CHITIETDONHANG set SoLuong = 7 where MaDH = N'DH0005' and MaSP = N'sp1001'
-update CHITIETDONHANG set SoLuong = -3 where MaDH = N'DH0005' and MaSP = N'sp1002'
-
-update SANPHAM set Gia = 20000  where MaSP = N'sp1001' 
-update SANPHAM set Gia = -10000 where MaSP = N'sp1002'
-
-select * from SANPHAM where MaSP = N'sp1001'
-select * from CHITIETDONHANG where MaDH = N'DH0005'
-select * from DONHANG
+select * from CHITIETDONHANG where MaDH = '1'
+select * from DONHANG where MaDH = '1'
+select * from SANPHAM where MaSP = '2001'
